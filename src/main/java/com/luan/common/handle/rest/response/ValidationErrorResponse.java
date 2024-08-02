2742045f3aee9c1639e352e8b81de8bd3a6060a1
Luan Coêlho <luancoelho.dev@gmail.com>
2024-08-02 18:42:11 -0300

    feat/fix: Criando packages no github.

--- src/main/java/com/luan/common/handle/rest/response/ValidationErrorResponse.java
+++ src/main/java/com/luan/common/handle/rest/response/ValidationErrorResponse.java
@@ -12,11 +12,11 @@ public class ValidationErrorResponse extends ErrorResponse {
 
     private Map<String, Object> invalidFields;
 
-    public void addInvalidField(String fieldName, Object errorMessage) {
+    public void addInvalidField(String field, Object errorMessage) {
         if (this.invalidFields == null) {
             this.invalidFields = new HashMap<>();
         }
-        this.invalidFields.put(fieldName, errorMessage);
+        this.invalidFields.put(field, errorMessage);
     }
 
 }
