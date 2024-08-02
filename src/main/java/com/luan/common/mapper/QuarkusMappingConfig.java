add3031b10c6d45bc1249e34856640aeca018372
Luan Coêlho <luancoelho.dev@gmail.com>
2024-08-02 18:49:26 -0300

    feat/fix: QuarkusMappingConfig público.

--- src/main/java/com/luan/common/mapper/QuarkusMappingConfig.java
+++ src/main/java/com/luan/common/mapper/QuarkusMappingConfig.java
@@ -3,6 +3,6 @@ package com.luan.common.mapper;
 import org.mapstruct.MapperConfig;
 
 @MapperConfig(componentModel = "cdi")
-interface QuarkusMappingConfig {
+public interface QuarkusMappingConfig {
 
 }
