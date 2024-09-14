08acb56013db79f9e634479ef05611f7212cd206
Luan Coêlho <luancoelho.dev@gmail.com>
2024-09-14 01:14:35 -0300

    refactor: Melhorando Exception Handle.

--- src/main/java/com/luan/common/dto/validador/constraints/PasswordValidationDto.java
+++
@@ -1,8 +0,0 @@
-package com.luan.common.dto.validador.constraints;
-
-import com.luan.common.validador.constraints.PasswordMatches;
-
-@PasswordMatches
-public record PasswordValidationDto(String password, String matchingPassword) {
-
-}
