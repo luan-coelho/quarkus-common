ae2788a343bff75d83eeb3e22339f940c0e47335
Luan Coêlho <73291345+luan-coelho@users.noreply.github.com>
2025-03-07 11:10:49 -0300

    fix: Correções.

--- src/main/java/com/luan/common/controller/user/UserController.java
+++ src/main/java/com/luan/common/controller/user/UserController.java
@@ -1,6 +1,5 @@
 package com.luan.common.controller.user;
 
-import com.itextpdf.styledxmlparser.jsoup.internal.StringUtil;
 import com.luan.common.controller.BaseController;
 import com.luan.common.dto.user.CreateAdminUserDto;
 import com.luan.common.dto.user.UserResponseDto;
@@ -8,14 +7,12 @@ import com.luan.common.model.user.User;
 import com.luan.common.service.module.ModuleService;
 import com.luan.common.service.user.UserService;
 import jakarta.inject.Inject;
-import jakarta.validation.Valid;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
-import java.util.Map;
 import java.util.UUID;
 
 @Path("/users")
@@ -29,28 +26,6 @@ public class UserController extends BaseController<User, UserResponseDto, UUID, 
     public Response save(CreateAdminUserDto dto) {
         User savedUser = service.save(dto);
         return Response.ok(savedUser).build();
-    }
-
-    @Path("/admin/exists/cpf/{cpf}")
-    @GET
-    public Response existsByCpf(@PathParam("cpf") String cpf) {
-        if (StringUtil.isBlank(cpf)) {
-            throw new IllegalArgumentException("CPF não pode ser nulo");
-        }
-        boolean exists = service.existsByCpf(cpf);
-        Map<String, Boolean> response = Map.of("exists", exists);
-        return Response.ok(response).build();
-    }
-
-    @Path("/admin/exists/email/{email}")
-    @GET
-    public Response existsByEmail(@PathParam("email") String email) {
-        if (StringUtil.isBlank(email)) {
-            throw new IllegalArgumentException("Email não pode ser nulo");
-        }
-        boolean exists = service.existsByEmail(email);
-        Map<String, Boolean> response = Map.of("exists", exists);
-        return Response.ok(response).build();
     }
 
     @GET
